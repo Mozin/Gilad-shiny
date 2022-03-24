c93337d4c3aa31c95aaea3346ae8f02382443444
Mohsin Vindhani <mohsinvindhani@192.168.1.5>
2022-03-25 01:01:07 +0530

    first commit

---
+++ app.R
@@ -0,0 +1,39 @@
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(ggplot2)
+
+source("data_app.R")
+
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(uiOutput("sidebarpanel")),
+  dashboardBody(
+    shinyjs::useShinyjs(),
+    uiOutput("body")
+  )
+)
+
+
+server <- function(input, output){
+  output$body <- renderUI({
+    tabItems(
+      tabItem(
+        tabName = "Data",
+        data_ui("data")
+      )
+    )
+  })
+  
+  output$sidebarpanel <- renderMenu({
+    sidebarMenu(
+      menuItem("Data", tabName = "Data")
+    )
+  })
+  
+  data_server("data")
+}
+
+
+shinyApp(ui = ui, server = server)
